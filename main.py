96da2d6c8a4a2e5d50a973615af44d1285612774
Harishankar-HDK <harisangarsm@gmail.com>
2025-03-01 12:16:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ def main():
         # Step 1: Get user input through speech-to-text
         user_input = get_user_input()
         
-        if user_input:
+        if user_input.lower()!='exit':
             # Step 2: Get AI model response
             response = get_model_response(user_input)
 
             clean_response=clean_model_output(response)
             print("AI Response:", clean_response)
             text_to_speech(clean_response)
-            
+        else:
+            print("Goodbye!")
+            break
 
 if __name__ == "__main__":
     main()
